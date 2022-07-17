1c36d44ca67185d746ff7ab3faff84474abaa8f4
Eric <chen519017928@qq.com>
2022-07-17 11:34:26 +0800

    fix url exposed

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
 
 
 module.exports = {
-    // mongoURI: "mongodb+srv://czh:<519017928Cc>@cluster0.82hl3w0.mongodb.net/?retryWrites=true&w=majority"
     mongoURI: "mongodb://localhost:27017/adminSystem",
     secret: "secret of node.js",
-    
 }
